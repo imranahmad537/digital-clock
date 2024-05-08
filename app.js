343c3dc4fb5ee4a0aea146e6bd646a9674399fba
Imran Ahmad <ahmadimrankh99@gmail.com>
2024-05-08 23:52:26 +0500

    first commit

--- app.js
+++ app.js
@@ -1,20 +1,51 @@
 
-let today = new Date();
-let year = today.getFullYear();
-let month = today.getMonth();
-let day = today.getMonth();
-let hour = today.getHours();
-let mint = today.getMinutes();
-let secds = today.getSeconds();
-
-let days = ["Sunday","Monday", "Tuesday","Wednesday","Thursday","Friday","Saturday"];
-let months = ["January","Februry","March","April","May","June","July","August","September","October","November","December"];
 
 
-document.querySelector(".yr").textContent = year
-document.querySelector(".mon").textContent = months[month];
-document.querySelector(".day").textContent = days[day];
-document.querySelector(".hr").textContent = hour;
-document.querySelector(".min").textContent = mint;
-document.querySelector(".sec").textContent = secds;
+
+function smartWatch(){
+    let today = new Date();
+    let hour = today.getHours().toString().padStart(2,'0');
+    let mint = today.getMinutes().toString().padStart(2,'0');
+    let secds = today.getSeconds().toString().padStart(2,'0');
+    let day = today.getDate();
+    let month = today.getMonth() +1;
+    let yr = today.getFullYear();
+
+    // Setting PM or AM
+    let meridiem = hour >=12 ? 'PM' : 'AM';
+    // setting 12 hour format
+    let thf = hour % 12 || 12;
+    
+    let timeString = `${thf}:${mint}:${secds}   ${meridiem}`;
+    
+    document.querySelector(".time").textContent = timeString;
+
+   let formatedDate = `${day}/${month}/${yr}`;
+   
+    let show = document.querySelector(".date");
+    show.textContent = formatedDate;
+}
+
+smartWatch()
+setInterval(smartWatch,1000);
+
+
+// let year = today.getFullYear();
+// let month = today.getMonth();
+// let day = today.getDay();
+
+
+
+
+
+// let days = ["Sunday","Monday", "Tuesday","Wednesday","Thursday","Friday","Saturday"];
+// let months = ["January","Februry","March","April","May","June","July","August","September","October","November","December"];
+
+// document.querySelector(".yr").textContent = year
+// document.querySelector(".mon").textContent = months[month];
+// document.querySelector(".day").textContent = days[day];
+// document.querySelector(".hr").textContent = hour;
+// document.querySelector(".min").textContent = mint;
+// document.querySelector(".sec").textContent = secds;
+
 
